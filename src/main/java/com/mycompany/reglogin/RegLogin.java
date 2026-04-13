a1f782fb0ca082f361c84f98f7e34a49a70a3a5e
MokoenaMohale <ST10483997@imconnect.edu.za>
2026-04-13 17:24:41 +0200

    Fourth commit

--- src/main/java/com/mycompany/reglogin/RegLogin.java
+++ src/main/java/com/mycompany/reglogin/RegLogin.java
@@ -15,7 +15,7 @@ public class RegLogin {
 
       
 
-
+// created main class and imported required libraries
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Login authSystem = new Login();
